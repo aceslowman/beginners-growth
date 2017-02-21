729ca4f5b2a0164115c876c581e83ce496dfc666
aceslowman <aceslowman@gmail.com>
2017-02-20 17:39:27 -0700

    Tree and traversal is WORKING, but has some issues with memory. Moving this to the main ofxGrowth project now.

--- linked-list-study/src/ofxGrowth.cpp
+++ linked-list-study/src/ofxGrowth.cpp
@@ -7,8 +7,6 @@ ofxGrowth::ofxGrowth(){
     density     = 0.5;
     depth       = 3;
     
-//    meshes.resize(depth);
-    
     root = new ofxGrowthNode(*this);
     root->growth_vector = ofVec3f(0,1,0);
     root->location = ofVec3f(0,0,0);
@@ -22,54 +20,54 @@ ofxGrowth::ofxGrowth(){
 void ofxGrowth::setupMesh(){
     int index = 0;
 
-    ofMesh temp_mesh;
-    temp_mesh.setMode(OF_PRIMITIVE_LINE_STRIP);
-    temp_mesh.setupIndicesAuto();
-    temp_mesh.addVertex(root->location);
-    temp_mesh.addColor(ofColor(0));
+    shared_ptr<ofMesh> shared_mesh = std::make_shared<ofMesh>();
+    shared_mesh->setMode(OF_PRIMITIVE_LINE_STRIP);
+    shared_mesh->setupIndicesAuto();
+    shared_mesh->addVertex(root->location);
+    shared_mesh->addColor(ofColor(0));
     
-    //I have to be assured that temp_mesh is inserted properly into shared_ptr, and THEN meshes
-    shared_ptr<ofMesh> shared_mesh(&temp_mesh);
     meshes.push_back(shared_mesh);
     
-    checkChildren(root, &temp_mesh);
+    checkChildren(root, shared_mesh.get());
 }
 
 //--------------------------------------------------------------
 void ofxGrowth::checkChildren(ofxGrowthNode * temp_node, ofMesh * temp_mesh){
+    
+    /*
+     I now have some degree of confirmation that the linked list is working, it's just that I now need to be sure that I'm traversing it correctly.
+     */
+    
     if(!temp_node->children.empty()){
-        for(int i = 0; i < temp_node->children.size(); i++){//because even when it is 0, I still grab the child?
+        for(int i = 0; i < temp_node->children.size(); i++){
             if(i > 0){
-        
-                add a new mesh (we have branched off)
-                push the new mesh pointer into the meshes vector
-                shared_ptr<ofMesh> new_mesh(new ofMesh);
+                shared_ptr<ofMesh> new_mesh = std::make_shared<ofMesh>();
                 
                 new_mesh->setMode(OF_PRIMITIVE_LINE_STRIP);
                 new_mesh->addVertex(temp_node->location);
                 new_mesh->addColor(ofFloatColor(0,0,0));
                 
                 meshes.push_back(new_mesh);
+   
+                checkChildren(temp_node->children[i].get(), new_mesh.get());//check all children of the child
             }else{
-        
+                /*
+                 When traversed through here, it works properly
+                 */
                 temp_mesh->addVertex(temp_node->location);
                 temp_mesh->addColor(ofFloatColor(0,0,0));
+                
+                ofxGrowthNode * temp_child = temp_node->children[i].get();
+                checkChildren(temp_child, temp_mesh);//check all children of the child
             }
-
+            temp_node = temp_node->children[i].get();
             num_nodes++;
-            
-            ofxGrowthNode * temp_child = temp_node->children[0].get();
-            
-            checkChildren(temp_child, temp_mesh);//check all children of the child
         }
-
     }
-    
 }
 
 //--------------------------------------------------------------
 void ofxGrowth::drawMesh(){
-//    for(auto & mesh : meshes){
     for(int i = 0; i < meshes.size(); i++){
         meshes[i].get()->draw();
     }
